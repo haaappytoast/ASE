f34de7321d983f7707bc8dabcabaefebed3f3c73
JeonghyunNam <ys49902kaist.ac.kr>
2023-02-26 10:21:04 +0000

    DeepMimic env (only orn reward) completed, delete testHumanoid.py

--- ase/env/tasks/humanoid_deepmimic.py
+++ ase/env/tasks/humanoid_deepmimic.py
@@ -7,10 +7,11 @@ from isaacgym import gymtorch
 
 from env.tasks.humanoid import Humanoid
 from utils import gym_util
-from utils.motion_lib import MotionLib
+from utils.motion_lib import MotionLib, DeepMimicMotionLib
 from isaacgym.torch_utils import *
 
 from utils import torch_utils
+from poselib.poselib.core import *
 
 class HumanoidDeepMimic(Humanoid):
     class StateInit(Enum):      
@@ -19,7 +20,6 @@ class HumanoidDeepMimic(Humanoid):
         Random = 2
 
     def __init__(self, cfg, sim_params, physics_engine, device_type, device_id, headless):
-        
         #! random state initialization set
         state_init = cfg["env"]["stateInit"]                                
         self._state_init = HumanoidDeepMimic.StateInit[state_init]                
@@ -34,7 +34,7 @@ class HumanoidDeepMimic(Humanoid):
         #! set reference motion
         self._motion_ids = torch.zeros(self.num_envs, device=self.device)
         self._motion_times = torch.zeros(self.num_envs, device=self.device)
-        self._phase = torch.zeros(self.num_envs, device=self.device)
+        self._phase = torch.zeros((self.num_envs, 1), device=self.device)
         motion_file = cfg['env']['motion_file']                             
         self._load_motion(motion_file)                                      
 
@@ -42,7 +42,7 @@ class HumanoidDeepMimic(Humanoid):
 
     def _setup_character_props(self, key_bodies):
         
-        #! TODO: deepmimic use axis-angle rotation representation, but use 6D first
+        #! use 6D for orientation
         asset_file = self.cfg["env"]["asset"]["assetFileName"]
 
         if (asset_file == "mjcf/amp_humanoid.xml"):
@@ -70,12 +70,12 @@ class HumanoidDeepMimic(Humanoid):
     def post_physics_step(self):
         self.progress_buf+=1
         time_elapsed = self._motion_times + self.progress_buf * self.dt
-        self._phase =  self._motion_lib._calc_phase(self._motion_ids, time_elapsed.to(self.device)).view(self.num_envs, -1)
+        self._phase =  self._motion_lib._calc_phase(self._motion_ids, time_elapsed.to(self.device)).view(self.num_envs, 1)
         self._refresh_sim_tensors()
         self._compute_observations()
         self._compute_reward(self.actions)
         self._compute_reset()
-        
+
         self.extras["terminate"] = self._terminate_buf
 
         # debug viz
@@ -87,7 +87,7 @@ class HumanoidDeepMimic(Humanoid):
     def _load_motion(self, motion_file):
         #! have to be changed
         assert(self._dof_offsets[-1] == self.num_dof)
-        self._motion_lib = MotionLib(motion_file=motion_file,
+        self._motion_lib = DeepMimicMotionLib(motion_file=motion_file,
                                      dof_body_ids=self._dof_body_ids,
                                      dof_offsets=self._dof_offsets,
                                      key_body_ids=self._key_body_ids.cpu().numpy(), 
@@ -116,7 +116,7 @@ class HumanoidDeepMimic(Humanoid):
 
     def _reset_ref_state_init(self, env_ids):
         num_envs = env_ids.shape[0]
-        motion_ids = self._motion_lib.sample_motions(num_envs)                                      #! get 
+        motion_ids = self._motion_lib.sample_motions(num_envs)                                    
         
         if (self._state_init == HumanoidDeepMimic.StateInit.Random):
             motion_times = self._motion_lib.sample_time(motion_ids)
@@ -138,7 +138,7 @@ class HumanoidDeepMimic(Humanoid):
         
         self._motion_ids = motion_ids
         self._motion_times = motion_times
-        self._phase[env_ids] = self._motion_lib._calc_phase(motion_ids, motion_times)
+        self._phase[env_ids] = self._motion_lib._calc_phase(motion_ids, motion_times).view(num_envs, 1)
         return
     
     def _set_env_state(self, env_ids, root_pos, root_rot, dof_pos, root_vel, root_ang_vel, dof_vel):
@@ -157,32 +157,40 @@ class HumanoidDeepMimic(Humanoid):
             body_rot = self._rigid_body_rot
             body_vel = self._rigid_body_vel
             body_ang_vel = self._rigid_body_ang_vel
+            _phase = self._phase
         else:
             body_pos = self._rigid_body_pos[env_ids]
             body_rot = self._rigid_body_rot[env_ids]
             body_vel = self._rigid_body_vel[env_ids]
             body_ang_vel = self._rigid_body_ang_vel[env_ids]
+            _phase = self._phase[env_ids]
         
         obs = build_deepmimic_observations(body_pos, body_rot, body_vel, body_ang_vel, self._local_root_obs,
-                                                self._root_height_obs, self._phase)
+                                                self._root_height_obs, _phase)
         return obs
 
-    def _get_humanoid_ref_obs(self):
-        root_pos, root_rot, dof_pos, root_vel, root_ang_vel, dof_vel, _ = self._motion_lib.get_motion_state(self._motion_ids, self._motion_times)
-        phase = self._motion_lib._calc_phase(self._motion_ids, self._motion_times)
-        obs_ref = build_deepmimic_ref_observation(root_pos, root_rot, dof_pos, root_vel, root_ang_vel, dof_vel, 
-                                                  self._local_root_obs, self._root_height_obs, phase)
-        return obs_ref
+    def _get_humanoid_ref_reward_obs(self):
+        body_orn, body_ang_vel = self._motion_lib.get_motion_body_state(self._motion_ids, self._motion_times)
+        return body_orn, body_ang_vel
     
-    def _compute_reward(self, env_ids=None):
-        obs_ref = self._get_humanoid_ref_obs()
-        orn_reward = self._compute_orn_reward(self.obs_buf, obs_ref, env_ids)
+    def _compute_reward(self, actions):
+        global_rot_ref, global_ang_vel_ref = self._get_humanoid_ref_reward_obs()
+        obs_ref= build_deepmimic_reward_observation(global_rot_ref, global_ang_vel_ref).to(self.device)
+        obs = build_deepmimic_reward_observation(self._rigid_body_rot, self._rigid_body_ang_vel)
+        orn_offset = 4*self.num_bodies
+        orn_reward = self._compute_orn_reward(obs[:, :orn_offset], obs_ref[:, :orn_offset])
+        # ang_vel = self._rigid_body_ang_vel
+        # ang_vel_reward = self._compute_ang_vel_reward(obs[:,orn_offset:], obs_ref[:,orn_offset:])
         return orn_reward
     
-    def _compute_orn_reward(self, obs, obs_ref,env_ids=None):
-        
-        return 0
+    def _compute_orn_reward(self, obs, obs_ref):
+        reward = calculate_humanoid_orn_reward(obs, obs_ref)
+        return reward
     
+    def _compute_ang_vel_reward(self, obs_ang_vel, obs_ref_ang_vel):
+        reward = calculate_humanoid_ang_vel_reward(obs_ang_vel, obs_ref_ang_vel)
+        return reward
+
 #####################################################################
 ###=========================jit functions=========================###
 #####################################################################
@@ -230,17 +238,42 @@ def build_deepmimic_observations(body_pos, body_rot, body_vel, body_ang_vel, loc
     local_body_ang_vel = flat_local_body_ang_vel.reshape(body_ang_vel.shape[0], body_ang_vel.shape[1] * body_ang_vel.shape[2])   
     
     # num_obs = 1 + (3 * 14) + (6 * 15) + (3 * 15) + (3 * 15) + 1  = 224
-    base_obs = torch.cat((root_h_obs, local_body_pos, local_body_rot_obs, local_body_vel, local_body_ang_vel), dim=-1)
-    obs = torch.cat((base_obs, phase_obs), dim=-1)
+    obs = torch.cat((root_h_obs, local_body_pos, local_body_rot_obs, local_body_vel, local_body_ang_vel, phase_obs), dim=-1)
+    # obs = torch.cat((base_obs, phase_obs), dim=0)
     
     return obs
-@torch.jit.script
-def build_deepmimic_ref_observation(root_pos, root_rot, dof_pos, root_vel, root_ang_vel, dof_vel, local_root_obs, root_height_obs, phase):
-    # type: (Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, bool, bool, Tensor) -> Tensor
-    pass
 
 @torch.jit.script
-def compute_humanoid_reward(obs_buf):
-    # type: (Tensor) -> Tensor
-    reward = torch.ones_like(obs_buf[:, 0]) 
+def build_deepmimic_reward_observation(body_rot, body_ang_vel):
+    # type: (Tensor, Tensor) -> Tensor
+    root_rot = body_rot[:, 0, :] 
+    heading_rot = torch_utils.calc_heading_quat_inv(root_rot)
+    heading_rot_expand = heading_rot.unsqueeze(-2)
+    heading_rot_expand = heading_rot_expand.repeat((1, body_rot.shape[1], 1))   
+    flat_heading_rot = heading_rot_expand.reshape(heading_rot_expand.shape[0] * heading_rot_expand.shape[1], 
+                                                    heading_rot_expand.shape[2])     
+    flat_body_rot = body_rot.reshape(body_rot.shape[0] * body_rot.shape[1], body_rot.shape[2])  
+    flat_local_body_rot = quat_mul(flat_heading_rot, flat_body_rot).reshape(body_rot.shape[0], -1)
+    
+    flat_body_ang_vel = body_ang_vel.reshape(body_ang_vel.shape[0] * body_ang_vel.shape[1], body_ang_vel.shape[2])   
+    flat_local_body_ang_vel = quat_rotate(flat_heading_rot, flat_body_ang_vel).reshape(body_ang_vel.shape[0], -1)                                       
+
+    obs = torch.cat((flat_local_body_rot, flat_local_body_ang_vel), dim=-1)
+    return obs
+
+@torch.jit.script
+def calculate_humanoid_orn_reward(obs_buf, obs_ref_buf):
+    # type: (Tensor, Tensor) -> Tensor
+    local_body_orn = obs_buf.reshape(-1, 4)
+    local_ref_body_orn = obs_ref_buf.reshape(-1, 4)
+    inv_local_body_orn = quat_inverse(local_body_orn)
+    diff_body_orn = quat_mul(inv_local_body_orn, local_ref_body_orn)
+    rot_diff_angle, _ = quat_angle_axis(diff_body_orn)
+    reward = torch.exp(-2 * (torch.sum(rot_diff_angle**2, dim=-1)))
+    return reward
+
+@torch.jit.script
+def calculate_humanoid_ang_vel_reward(obs_buf, obs_ref_buf):
+    # type: (Tensor, Tensor) -> Tensor
+    reward = torch.exp(-0.05*torch.sum((obs_buf- obs_ref_buf)**2, dim=1))
     return reward
