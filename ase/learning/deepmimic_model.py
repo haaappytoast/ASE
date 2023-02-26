cca1f6035a8a22256864922ae7b4c321ea5a2f12
JeonghyunNam <ys49902kaist.ac.kr>
2023-02-26 12:18:04 +0000

    DeepMimic Model

---
+++ ase/learning/deepmimic_model.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2018-2022, NVIDIA Corporation
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import torch.nn as nn
+from rl_games.algos_torch.models import ModelA2CContinuousLogStd
+
+class ModelDeepMimicContinuous(ModelA2CContinuousLogStd):
+    def __init__(self, network):
+        #! network = learning.deepmm_network_builder.DeepmmBuilder
+        super().__init__(network)
+        return
+
+    def build(self, config):
+        #!! changed
+        #! self.network_builder = -> learning.deepmm_network_builder.DeepmmBuilder -> by ModelA2CContinuousLogStd.init()
+        net = self.network_builder.build('deepmimic', **config)
+        for name, _ in net.named_parameters():
+            print(name)
+        #! net => deemm_network_builder.build()로 인해, DeepmmBuilder.Network(self.params, **kwargs) 가 만들어짐.
+        return ModelDeepMimicContinuous.Network(net)   #! initialize 하는 것
+
+    class Network(ModelA2CContinuousLogStd.Network):
+        def __init__(self, a2c_network):
+            #! a2c_network => DeepmmBuilder.Network
+            super().__init__(a2c_network)
+            return
+
+        def forward(self, input_dict):
+            is_train = input_dict.get('is_train', True)
+            #! 2. go to deepmm_network_builder.py Network.forward
+            result = super().forward(input_dict)
+            
+            return result
